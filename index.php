f3f2836ca9a7cca56004e3391d4327425b521fc3
Chris French <chris@ammonitenetworks.com>
2013-07-26 17:59:31 -0400

    pusher demo

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+$f3=require('lib/base.php');
+$f3->set('AUTOLOAD','pusherserver/lib/');
+$f3->config('config.ini');
+
+ 
+
+	
+
+$f3->route('GET /',
+	function($f3) {
+		echo 'workin';
+	}
+);
+
+
+$f3->route('GET /game',
+	function($f3) {
+		echo View::instance()->render('game.htm');
+	}
+);
+
+$f3->route('POST /controller/input',
+	function($f3) {
+$app_id = '50411';
+$app_key = 'f2b24963b454eac2cfd7';
+$app_secret = '47bc64271eb245aadb71';
+
+$pusher = new Pusher( $app_key, $app_secret, $app_id );
+		$pusher->trigger('game', $f3->get('POST.key'), 'server knows you pressed '. $f3->get('POST.key'));
+		
+	}
+);
+
+
+$f3->route('GET /controller/input',
+	function($f3) {
+	
+		$pusher->trigger('game', $f3->get('POST.key'), 'server knows you loaded input ');
+		
+	}
+);
+
+
+$f3->run();
---
+++ ui/js/game.js
@@ -0,0 +1,30 @@
+ $(document).ready(function() {
+ 
+
+
+ $(".controllerButton").click(function() {
+  		console.log('button was pressed');
+  		console.log(this.id);
+  		call = '/controller/input';
+  		console.log(call);
+  		$.post(call, { key: this.id, type: 'press'});
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
