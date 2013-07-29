d3b599864736411c2ed917fcb7d3c9c9c78853d0
Chris French <chris@ammonitenetworks.com>
2013-07-29 10:32:48 -0600

    controller updates

--- ui/js/controller.js
+++ ui/js/controller.js
@@ -8,21 +8,6 @@
   		$.post(call, { key: this.id, type: 'press'});
 	});
 
-  $(window).bind("devicemotion", function(e){
-                    var movitBaby = e.originalEvent,
-                        acelera = movitBaby.accelerationIncludingGravity,
-                        x = acelera.x,
-                        y = acelera.y,
-                        z = acelera.z;
- 
-
-                        //TODO here we would put a function that would detect the swing event and luanch an event to the server to dispatch to the  screen client
-
-                    $("#game").html(
-                        "x = <b>" + x + "</b><br/>" +
-                        "y = <b>" + y + "</b><br/>" +
-                        "z = <b>" + z + "</b>"
-                    );
-                });
+  
 
 });
